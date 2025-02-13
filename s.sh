a62524b5b337df0e126ecce16d510272e4d5b482
...:::01000101:::... <58969724+SourceLab081@users.noreply.github.com>
2025-02-13 10:48:49 +0700

    Create s.sh

---
+++ s.sh
@@ -0,0 +1,5 @@
+mkdir bckp_;cd bckp_
+wget https://github.com/SourceLab081/files/raw/refs/heads/main/30.0.ignore.cil && mv 30.0.ignore.cil ../system/sepolicy/private/compat/30.0/30.0.ignore.cil
+wget https://github.com/SourceLab081/files/raw/refs/heads/main/31.0.ignore.cil && mv 31.0.ignore.cil ../system/sepolicy/private/compat/31.0/31.0.ignore.cil
+wget https://github.com/SourceLab081/files/raw/refs/heads/main/32.0.ignore.cil && mv 32.0.ignore.cil ../system/sepolicy/private/compat/32.0/32.0.ignore.cil
+cd ..
