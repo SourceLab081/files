22ca7d7bd3321d867edabbabfb4e9e75a03d0224
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-04-04 03:25:05 +0700

    sync again coz dt updated

--- komodo_a15.sh
+++ komodo_a15.sh
@@ -2,10 +2,10 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 echo "start date = `date`"
-#repo init --depth 1 -u https://github.com/Komodo-OS/manifest -b 15 --git-lfs
-#rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b 15-Komodo .repo/local_manifests 
-#echo "repo sync" 
-#/opt/crave/resync.sh 
+repo init --depth 1 -u https://github.com/Komodo-OS/manifest -b 15 --git-lfs
+rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b 15-Komodo .repo/local_manifests 
+echo "repo sync" 
+/opt/crave/resync.sh 
 . fix_chromium.sh
 rm -rf hardware/xiaomi/aidl/sensors
 echo "envsetup.sh" 
