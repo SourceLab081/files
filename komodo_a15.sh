3f234d75b0eb54c2c3a0b019dcc482fe47cf001b
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-04-03 10:48:20 +0700

    Not resync again

--- komodo_a15.sh
+++ komodo_a15.sh
@@ -2,11 +2,11 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 echo "start date = `date`"
-repo init --depth 1 -u https://github.com/Komodo-OS/manifest -b 15 --git-lfs
-rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b 15-Komodo .repo/local_manifests 
-echo "repo sync" 
-/opt/crave/resync.sh 
-#rm -rf hardware/xiaomi/aidl/sensors
+#repo init --depth 1 -u https://github.com/Komodo-OS/manifest -b 15 --git-lfs
+#rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b 15-Komodo .repo/local_manifests 
+#echo "repo sync" 
+#/opt/crave/resync.sh 
+##rm -rf hardware/xiaomi/aidl/sensors
 echo "envsetup.sh" 
 source build/envsetup.sh 
 #export SELINUX_IGNORE_NEVERALLOWS=true
