a8ba148332b93d4d9b56b815d4ec38d5bc466ccb
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-04-04 13:19:22 +0700

    Update HorizonDroid_a15.sh add sign key

--- HorizonDroid_a15.sh
+++ HorizonDroid_a15.sh
@@ -6,6 +6,11 @@ repo init --depth 1 -u https://github.com/HorizonDroidLab/manifest.git -b fiftee
 rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b 15-HorizonDroidLab .repo/local_manifests 
 echo "repo sync" 
 /opt/crave/resync.sh 
+echo "add sign key"
+export subject='/C=ID/ST=DKI Jakarta/L=Jakarta/O=Android/OU=Android/CN=rom/emailAddress=craveio0explore@gmail.com'
+for x in releasekey platform shared media networkstack verity otakey testkey sdk_sandbox bluetooth nfc; do \
+    yes "" | ./development/tools/make_key vendor/horizon/signing/keys/$x "$subject"; \
+done
 echo "envsetup.sh" 
 source build/envsetup.sh 
 echo "breakfast or lunch" 
