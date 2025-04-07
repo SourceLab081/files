9c3034d440bb82be068545e1072bbdec4567562a
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-04-07 07:30:02 +0700

    Update komodo_a15.sh

--- komodo_a15.sh
+++ komodo_a15.sh
@@ -6,14 +6,6 @@ repo init --depth 1 -u https://github.com/Komodo-OS/manifest -b 15 --git-lfs
 rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b 15-Komodo .repo/local_manifests 
 echo "repo sync" 
 /opt/crave/resync.sh 
-# signing key
-mkdir -p vendor/extra
-cd vendor/extra && wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.8/sign.zip && unzip sign.zip && rm sign.zip
-cd ../..
-# not using new hardware/xiaomi/aidl/sensors 
-wget https://github.com/SourceLab081/uploadz/releases/download/v0.1.3/sens.zip
-wget https://github.com/SourceLab081/uploadz/releases/download/v0.1.3/sensors.sh
-. sensors.sh
 echo "envsetup.sh" 
 source build/envsetup.sh 
 echo "breakfast or lunch" 
