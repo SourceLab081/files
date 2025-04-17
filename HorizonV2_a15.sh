cf6e40b4dc1caa2186948a19a43da8a09f487b7c
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-04-18 06:15:34 +0700

    Create HorizonV2_a15.sh

---
+++ HorizonV2_a15.sh
@@ -0,0 +1,15 @@
+export TZ=Asia/Jakarta 
+export BUILD_USERNAME=sourceslab062
+export BUILD_HOSTNAME=foss
+echo "start date = `date`"
+repo init --depth 1 -u https://github.com/HorizonV2/android.git -b lineage-22.2 --git-lfs
+rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b 15-HorizonV2 .repo/local_manifests 
+echo "repo sync" 
+/opt/crave/resync.sh 
+echo "envsetup.sh" 
+source build/envsetup.sh 
+echo "breakfast or lunch" 
+lunch lineage_fog-bp1a-userdebug
+echo "Build the code" 
+make installclean
+mka horizon
