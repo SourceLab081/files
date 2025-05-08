dd58a6a474915c8e2616efdb3dc2c196ee8077c3
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-05-08 23:33:39 +0700

    Update komodo_a15.sh

--- komodo_a15.sh
+++ komodo_a15.sh
@@ -2,10 +2,10 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 echo "start date = `date`"
-repo init --depth 1 -u https://github.com/Komodo-OS/manifest -b 15 --git-lfs
-rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b 15-Komodo .repo/local_manifests 
-echo "repo sync" 
-/opt/crave/resync.sh 
+#repo init --depth 1 -u https://github.com/Komodo-OS/manifest -b 15 --git-lfs
+#rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b 15-Komodo .repo/local_manifests 
+#echo "repo sync" 
+#/opt/crave/resync.sh 
  ## signing key
  #mkdir -p vendor/extra
  #cd vendor/extra && wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.8/sign.zip && unzip sign.zip && rm sign.zip
