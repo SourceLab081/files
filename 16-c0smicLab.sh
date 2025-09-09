c58c78d2a3a94236176430e802b92ff845232606
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-09-09 20:09:38 +0700

    Update 16-c0smicLab.sh

--- 16-c0smicLab.sh
+++ 16-c0smicLab.sh
@@ -1,7 +1,8 @@
-xport TZ=Asia/Jakarta
+export TZ=Asia/Jakarta
 export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
-rm -rf device/xiaomi/fog
+
+# rm -rf device/xiaomi/fog
 repo init --depth 1 -u https://github.com/c0smic-Lab/manifest.git -b 16 --git-lfs
 rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-c0smicLab .repo/local_manifests
 echo "repo sync"
@@ -17,7 +18,14 @@ if [ ! -f script_sch3.sh ]; then
    wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/script_sch3.sh
 fi
 . script_sch3.sh
-#rm -rf out/target/product/fog/system/etc/vintf
+# rm -rf out/target/product/fog/system/etc/vintf
+# fix for error Problems processing genfscon rules
+# https://github.com/LineageOS/android_device_qcom_sepolicy_vndr/blob/lineage-22.2-legacy-um/generic/vendor/common/init_shell.te
+fldr="device/qcom/sepolicy_vndr/legacy-um/generic/vendor/common/"
+wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/file.te && mv file.te $fldr
+wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/genfs_contexts && mv genfs_contexts $fldr
+wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/init_shell.te && mv init_shell.te $fldr
+
 echo "envsetup.sh"
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true 
