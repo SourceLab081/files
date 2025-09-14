7f032470846d27054f49ed406fda7f5effa38a16
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-09-15 04:26:44 +0700

    Update 16-halogenOS.sh

--- 16-halogenOS.sh
+++ 16-halogenOS.sh
@@ -19,6 +19,8 @@ if [ ! -f script_sch3.sh ]; then
 fi
 . script_sch3.sh
 echo "This is for testing only"
+sudo apt --yes install libimagequant0 libraqm0
+sudo apt --yes install python3-pil
 file0="http://ftp.debian.org/debian/pool/main/p/pillow/python3-pil_11.1.0-5+b1_amd64.deb"
 file1="http://ftp.debian.org/debian/pool/main/libi/libimagequant/libimagequant0_2.18.0-1+b2_amd64.deb"
 file2="http://ftp.debian.org/debian/pool/main/r/raqm/libraqm0_0.10.2-1_amd64.deb"
