8df09da502112ecdb4d859153611835a1a57a079
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-09-20 14:32:08 +0700

    Update 16-yaap.sh

--- 16-yaap.sh
+++ 16-yaap.sh
@@ -3,7 +3,7 @@ export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 export TARGET_BOARD_PLATFORM=bengal
 
-# rm -rf device/xiaomi/fog
+rm -rf device/xiaomi/fog
 repo init --depth 1 -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
 rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-yaap .repo/local_manifests
 echo "repo sync"
