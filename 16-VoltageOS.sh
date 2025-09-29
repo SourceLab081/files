3d0332a956e998b3e361d3e7d6c938426ce6c35d
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-09-29 15:47:43 +0700

    Update 16-VoltageOS.sh

--- 16-VoltageOS.sh
+++ 16-VoltageOS.sh
@@ -2,16 +2,16 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 
-#rm -rf device/xiaomi/fog
+rm -rf device/xiaomi/fog
 repo init --depth 1 -u https://github.com/VoltageOS/manifest.git -b 16 --git-lfs 
 rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-VoltageOS .repo/local_manifests
 echo "repo sync"
 /opt/crave/resync.sh
 # signing key
 curDir=`pwd`
-#cd vendor/voltage-priv/keys
-#./keys.sh
-#cd $curDir
+cd vendor/voltage-priv/keys
+./keys.sh
+cd $curDir
 #if [ ! -d vendor/extra ]; then
 #   mkdir -p vendor/extra
 #   cd vendor/extra && wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.8/sign.zip && unzip sign.zip && rm sign.zip
@@ -23,9 +23,15 @@ if [ ! -f script_sch2.sh ]; then
 fi
 . script_sch2.sh
 
+if [ -f out/target/product/fog/build_thumbprint.txt ]; then
+   export headS="Redmi/VoltageOS_fog/fog:"
+   export tailS=`cat out/target/product/fog/build_thumbprint.txt`
+   echo "$headS$tailS" > out/target/product/fog/build_fingerprint.txt 
+else
+   wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/build_fingerprint.txt && mv build_fingerprint.txt out/target/product/fog/
+fi
 
-wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/build_fingerprint.txt && mv build_fingerprint.txt out/target/product/fog/
-# cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
+cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
 
 # rm -rf out/target/product/fog/system/etc/vintf
 # fix for error Problems processing genfscon rules
