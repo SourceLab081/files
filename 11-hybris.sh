689d0fb8537df60ca00e5c1262d4d44d2b90e2df
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-06 08:18:31 +0700

    Update local_manifests repository URL in script

--- 11-hybris.sh
+++ 11-hybris.sh
@@ -3,7 +3,7 @@ export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 echo "start date = `date`"
 repo init --depth 1 -u https://github.com/SailfishOS-miatoll/android  -b hybris-18.1 --git-lfs --no-clone-bundle
-rm -rf .repo/local_manifests && git clone https://gitlab.com/sourceslab062/local_manifests --depth 1 -b hybris-18.1 .repo/local_manifests
+rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b hybris-18.1 .repo/local_manifests
 echo "repo sync"
 /opt/crave/resync.sh
 # signing key
