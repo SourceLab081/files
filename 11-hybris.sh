a82e92a403bdc72158a69c1541614a938beaf4de
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-08 13:29:59 +0700

    Update 11-hybris.sh

--- 11-hybris.sh
+++ 11-hybris.sh
@@ -2,7 +2,7 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 echo "start date = `date`"
-repo init --depth 1 -u https://github.com/SailfishOS-miatoll/android  -b hybris-18.1 --git-lfs --no-clone-bundle
+repo init --depth 1 -u hhttps://github.com/mer-hybris/android.git  -b hybris-18.1 --git-lfs --no-clone-bundle
 rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b hybris-18.1 .repo/local_manifests
 echo "repo sync"
 /opt/crave/resync.sh
@@ -10,7 +10,16 @@ echo "repo sync"
 #mkdir -p vendor/extra
 #cd vendor/extra && wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.8/sign.zip && unzip sign.zip && rm sign.zip
 #cd ../..
-#export SAILFISH_BUILD=1
+export SAILFISH_BUILD=1
+curDir=`pwd`
+export ANDROID_ROOT=`pwd`
+cd external/chromium-webview;rm Android.mk;ln -s patches/os_pickup.mk Android.mk;cd $curDir;
+echo "apply patch"
+. hybris-patches/apply-patches.sh
+cd $ANDROID_ROOT
+. fog-patches/fog_patches.sh
+cd $curDir
+
 echo "envsetup.sh"
 . build/envsetup.sh
 unset TARGET_FORCE_PREBUILT_KERNEL
@@ -18,5 +27,6 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 echo "breakfast/lunch"
 breakfast fog userdebug
-echo "start build..."
-make -j$(nproc --all) hybris-hal droidmedia libui_compat_layer libsfplugin_ccodec
+echo "start the build..."
+make -j$(nproc --all) hybris-hal droidmedia 
+# libui_compat_layer libsfplugin_ccodec
