d189dcf55a2f83c929f37886d0cc2e9048221230
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-09 21:41:59 +0700

    Update 11-hybris.sh

--- 11-hybris.sh
+++ 11-hybris.sh
@@ -2,10 +2,11 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 echo "start date = `date`"
-repo init --depth 1 -u https://github.com/SailfishOS-msmnile/manifest.git -b hybris-18.1 --git-lfs 
+repo init --depth 1 -u https://github.com/SailfishOS-msmnile/manifest.git -b hybris-18.1 
 rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b hybris-18.1 .repo/local_manifests
 echo "repo sync"
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
+repo sync --fetch-submodules -j$(nproc --all)
 # signing key
 #mkdir -p vendor/extra
 #cd vendor/extra && wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.8/sign.zip && unzip sign.zip && rm sign.zip
@@ -18,20 +19,21 @@ curDir=`pwd`
 export ANDROID_ROOT=`pwd`
 #cd external/chromium-webview;rm Android.mk;ln -s patches/os_pickup.mk Android.mk;cd $curDir;
 # echo "apply patch"
-wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/chromium.zip;unzip -o chromium.zip -d external/chromium-webview/;rm chromium.zip 
+#wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/chromium.zip;unzip -o chromium.zip -d external/chromium-webview/;rm chromium.zip 
 wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/droid-hal-device.inc;mv droid-hal-device.inc rpm/dhd/
-#. hybris-patches/apply-patches.sh  --mb
-#cd $ANDROID_ROOT
+. hybris-patches/apply-patches.sh  --mb
+cd $ANDROID_ROOT
 #. fog-patches/fog_patches.sh
 #cd $curDir
 
 echo "envsetup.sh"
-. build/envsetup.sh
-unset TARGET_FORCE_PREBUILT_KERNEL
+source build/envsetup.sh
+
 export ALLOW_MISSING_DEPENDENCIES=true 
 export SELINUX_IGNORE_NEVERALLOWS=true
+
 echo "breakfast/lunch"
 breakfast $DEVICE
-echo "start the build..."
+echo "start compiling..."
 make -j$(nproc --all) hybris-hal droidmedia 
 # libui_compat_layer libsfplugin_ccodec
