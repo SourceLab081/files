c75abeffcddbac681d299885a9731349aab9fea9
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-12 05:40:51 +0700

    Update 16-VoltageOS.sh

--- 16-VoltageOS.sh
+++ 16-VoltageOS.sh
@@ -2,16 +2,16 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 
-rm -rf device/xiaomi/fog
-repo init --depth 1 -u https://github.com/VoltageOS/manifest.git -b 16 --git-lfs 
-rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-VoltageOS .repo/local_manifests
-echo "repo sync"
-/opt/crave/resync.sh
+#rm -rf device/xiaomi/fog
+#repo init --depth 1 -u https://github.com/VoltageOS/manifest.git -b 16 --git-lfs 
+#rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-VoltageOS .repo/local_manifests
+#echo "repo sync"
+#/opt/crave/resync.sh
 # signing key
 curDir=`pwd`
-cd vendor/voltage-priv/keys
-./keys.sh
-cd $curDir
+#cd vendor/voltage-priv/keys
+#./keys.sh
+#cd $curDir
 #if [ ! -d vendor/extra ]; then
 #   mkdir -p vendor/extra
 #   cd vendor/extra && wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.8/sign.zip && unzip sign.zip && rm sign.zip
