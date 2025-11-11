17d1126ba242f4ec381c80a79de72fbca16995bf
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-11 20:22:25 +0700

    Update 11-hybris.sh

--- 11-hybris.sh
+++ 11-hybris.sh
@@ -5,8 +5,8 @@ echo "start date = `date`"
 repo init --depth 1 -u https://github.com/SailfishOS-msmnile/manifest.git -b hybris-18.1 
 rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b hybris-18.1 .repo/local_manifests
 echo "repo sync"
-#/opt/crave/resync.sh
-repo sync --fetch-submodules -j$(nproc --all)
+/opt/crave/resync.sh
+#repo sync --fetch-submodules -j$(nproc --all)
 # signing key
 #mkdir -p vendor/extra
 #cd vendor/extra && wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.8/sign.zip && unzip sign.zip && rm sign.zip
