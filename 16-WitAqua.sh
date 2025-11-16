fa86e83e8156f2ca110dec8807f4989479b3665b
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-16 11:36:31 +0700

    Update 16-WitAqua.sh

--- 16-WitAqua.sh
+++ 16-WitAqua.sh
@@ -3,6 +3,7 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 echo "start date = `date`"
+rm -rf kernel/xiaomi/fog
 repo init --depth 1 -u https://github.com/WitAqua/manifest.git -b 16.0 --git-lfs 
 rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-WitAqua .repo/local_manifests
 echo "repo sync"
@@ -19,19 +20,20 @@ if [ ! -d vendor/extra ]; then
 fi
 
 curDir=`pwd`
-if [ ! -f external/chromium-webview/prebuilt/prebuilt.zip ]; then
-   wget https://github.com/SourceLab081/uploadz/releases/download/v0.1.8/prebuilt.zip
-   mv prebuilt.zip external/chromium-webview/prebuilt/
+if [ ! -f external/chromium-webview/prebuilt/arm64/webview.apk ]; then
+   if [ ! -f external/chromium-webview/prebuilt/prebuilt.zip ]; then
+       wget https://github.com/SourceLab081/uploadz/releases/download/v0.1.8/prebuilt.zip
+       mv prebuilt.zip external/chromium-webview/prebuilt/
+   fi
+   cd external/chromium-webview/prebuilt/;unzip -o prebuilt.zip;cd $curDir
 fi
-cd external/chromium-webview/prebuilt/;unzip -o prebuilt.zip;cd $curDir
-
 
 if [ ! -f script_sch2.sh ]; then
    wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/script_sch2.sh
 fi
 . script_sch2.sh
 
-cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
+#cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
 
 echo "envsetup.sh"
 . build/envsetup.sh
