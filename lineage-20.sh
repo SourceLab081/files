8a0042c07e0e94f670c64edb9ccf8cba5ee9b890
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-12-21 23:56:48 +0700

    Modify lineage-20.sh to adjust build setup
    Comment out removal of fog directories and add symbolic links for ncurses and tinfo libraries.

--- lineage-20.sh
+++ lineage-20.sh
@@ -4,11 +4,13 @@ export BUILD_HOSTNAME=foss
 export non_hybris=yes
 echo "start date = `date`" > date.txt
 
-rm -rf device/xiaomi/fog vendor/xiaomi/fog
+#rm -rf device/xiaomi/fog vendor/xiaomi/fog
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
 rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b lineage-20 .repo/local_manifests 
 echo "repo sync" 
 /opt/crave/resync.sh
+sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6.5 /usr/lib/x86_64-linux-gnu/libncurses.so.5 
+sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6.5 /usr/lib/x86_64-linux-gnu/libtinfo.so.5 
 #/opt/crave/resync.sh 
 # wget https://raw.githubusercontent.com/accupara/docker-images/master/aosp/common/resync.sh
 # . resync.sh
