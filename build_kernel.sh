fc638dae45c7e5fe72d92b74d7e9738912507664
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-12-25 10:07:12 +0700

    update compile kernel

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@
 # Copyright (C) 2020-2021 Adithya R.
 # modified by @teleg3_7
 
-sudo apt update && sudo apt install -y libc++-dev build-essential git bc kmod cpio flex cpio libncurses5-dev bison libssl-dev libelf-dev curl unzip
+sudo apt update && sudo apt install -y libc++-dev build-essential git bc kmod cpio flex cpio libncurses5-dev bison libssl-dev libelf-dev curl zip unzip
 
 kernel_src="--depth=1 -b fog-new https://github.com/SourceLab081/greenforce"
 config="vendor/fog-hybris_defconfig"
@@ -28,7 +28,6 @@ git clone $kernel_src kernel
 wget https://github.com/SourceLab081/files/raw/refs/heads/main/telegramUploader.sh && chmod +x telegramUploader.sh
 git clone --depth=1 -b 14 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"
 git clone --depth=1 -b master https://github.com/CHRISL7/AnyKernel3 "$AK3_DIR";
-set +x
 
 mkdir -p kernel/out
 
@@ -54,12 +53,14 @@ dtb="out/arch/arm64/boot/dtb.img"
 dtbo="out/arch/arm64/boot/dtbo.img"
 
 if [ -f "$kernel" ]; then
+    echo  "\nCompilation success"
 	cp $kernel $dtb $dtbo $AK3_DIR
 	cd $AK3_DIR && zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
 	cd $curDir
-	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo  "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
          ./telegramUploader.sh  folds/$ZIPNAME 
 else
-	echo -e "\nCompilation failed!"
-	exit 1
+	echo  "\nCompilation failed!"
+	#exit 1
 fi
+set +x
