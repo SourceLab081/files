07d3e788867caf5af13ec8f637fd054ed9fa2880
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-12-26 17:04:54 +0700

    Create uploadToGithub.sh

---
+++ uploadToGithub.sh
@@ -0,0 +1,15 @@
+# how to use : . uploadToGithub.sh filename
+wget https://github.com/github-release/github-release/releases/download/v0.10.0/linux-amd64-github-release.bz2
+bunzip2 linux-amd64-github-release.bz2
+mv linux-amd64-github-release github-release
+chmod +x github-release
+
+# change the tag if not for kernel
+tag='v0.2.2'
+./github-release upload \
+    -s $SECRET_NUM \
+    -u SourceLab081 \
+    -r uploadz \
+    -t $tag \
+    -n $1 \
+    -f $1' 
