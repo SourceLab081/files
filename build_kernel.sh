c3ef0319b77126dde817f57b9e00ada92a6cb3f6
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-12-30 05:22:09 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -44,7 +44,7 @@ if [ "$KSU_NEXT" = "yes" ]; then
     #wget https://github.com/SourceLab081/files/raw/refs/heads/main/patch_ksu.sh
 	#. patch_ksu.sh
 	echo "CONFIG_KSU=y" >> arch/arm64/configs/$DEFCONFIG
-	rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - 
+	#rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - 
 	#curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
 	rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
 	#wget https://github.com/SourceLab081/files/raw/refs/heads/main/fix_ksuNext.sh
