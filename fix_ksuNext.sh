e3dbe691f3b498f6ab8ad180840c71cd44557b7e
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-12-29 08:39:10 +0700

    Update fix_ksuNext.sh

--- fix_ksuNext.sh
+++ fix_ksuNext.sh
@@ -1,2 +1,3 @@
 sed -i "12i\#include <linux/sched/task.h>" KernelSU-Next/kernel/allowlist.c
+sed -i "11i\#include \"kernel_compat.h\"" KernelSU-Next/kernel/allowlist.c
 sed -i "6i\#include \"kernel_compat.h\"" KernelSU-Next/kernel/setuid_hook.h
