9f1029245885f7b014f6d9ea2196fab11d940b38
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2026-01-08 00:52:58 +0700

    Update uploadToGithub.sh

--- uploadToGithub.sh
+++ uploadToGithub.sh
@@ -1,9 +1,10 @@
 # how to use : . uploadToGithub.sh filename
-wget https://github.com/github-release/github-release/releases/download/v0.10.0/linux-amd64-github-release.bz2
-bunzip2 linux-amd64-github-release.bz2
-mv linux-amd64-github-release github-release
-chmod +x github-release
-
+if [ ! -f github-release ]; then
+   wget https://github.com/github-release/github-release/releases/download/v0.10.0/linux-amd64-github-release.bz2
+   bunzip2 linux-amd64-github-release.bz2
+   mv linux-amd64-github-release github-release
+   chmod +x github-release
+fi
 # change the tag if not for kernel
 tag='v0.2.2'
 ./github-release upload \
