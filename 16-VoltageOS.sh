ccd93631e98be4436b0b4ee44319cc78ae275cad
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2026-01-17 17:53:04 +0700

    Update 16-VoltageOS.sh

--- 16-VoltageOS.sh
+++ 16-VoltageOS.sh
@@ -4,7 +4,7 @@ export BUILD_HOSTNAME=foss
 
 #rm -rf device/xiaomi/fog
 rm -rf external/chromium-webview/prebuilt/*
-repo init --depth 1 -u https://github.com/VoltageOS/manifest.git -b 16 --git-lfs 
+repo init --depth 1 -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs 
 rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-VoltageOS .repo/local_manifests
 echo "repo sync"
 /opt/crave/resync.sh
