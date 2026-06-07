6e0d0232c85eb652bbe814683e1b511d95c46a51
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-06-07 08:08:13 +0700

    Update symbolic links for ncurses and tinfo libraries

--- 11-lineage.sh
+++ 11-lineage.sh
@@ -18,8 +18,8 @@ curDir=`pwd`
 FILE="/usr/lib/x86_64-linux-gnu/libncurses.so.5"
 if [ ! -f "$FILE" ]; then
   echo "File '$FILE' does not exist."
-  sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5 
-  sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5 
+  sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5 
+  sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5 
 fi
 
 #cd $curDir
