c96a9716ff71d163283cdbbdeadba3bfef799139
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-06-13 03:29:44 +0700

    update vos-16

--- 16-VoltageOS.sh
+++ 16-VoltageOS.sh
@@ -4,25 +4,25 @@ export BUILD_HOSTNAME=foss
 
 #rm -rf device/xiaomi/fog
 
-##repo init --depth 1 -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs 
-##rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-VoltageOS .repo/local_manifests
-##echo "repo sync"
-##opt/crave/resync.sh
+repo init --depth 1 -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs 
+rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-VoltageOS .repo/local_manifests
+echo "repo sync"
+opt/crave/resync.sh
 # signing key
 curDir=`pwd`
-##cd vendor/voltage-priv/keys
-##./keys.sh
-##cd $curDir
+cd vendor/voltage-priv/keys
+./keys.sh
+cd $curDir
 
-##if [ ! -f script_sch2.sh ]; then
- ##  wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/script_sch2.sh
-##fi
-##. script_sch2.sh
+if [ ! -f script_sch2.sh ]; then
+   wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/script_sch2.sh
+fi
+. script_sch2.sh
 
 ##wget https://github.com/SourceLab081/uploadz/releases/download/v0.1.8/voltage.devices && mv voltage.devices vendor/voltage/
 #cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
 #cd kernel/xiaomi/fog &&	rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy_susfs && cd $curDir
-##rm -rf kernel/xiaomi/fog && git clone  -b fog-new3 --depth 1 --recurse-submodules https://github.com/SourceLab081/greenforce kernel/xiaomi/fog
+rm -rf kernel/xiaomi/fog && git clone  -b fog_new --depth 1 --recurse-submodules https://github.com/SourceLab081/greenforce kernel/xiaomi/fog
 # rm -rf out/target/product/fog/system/etc/vintf
 # fix for error Problems processing genfscon rules
 # https://github.com/LineageOS/android_device_qcom_sepolicy_vndr/blob/lineage-22.2-legacy-um/generic/vendor/common/init_shell.te
