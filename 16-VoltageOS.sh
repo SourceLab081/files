85d96e7ab3fc9b8acd860fc9961adfe838022da3
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-07-09 10:48:09 +0700

    Comment out initialization commands in VoltageOS script
    Commented out various commands for initialization and setup.

--- 16-VoltageOS.sh
+++ 16-VoltageOS.sh
@@ -3,18 +3,18 @@ export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 
 #rm -rf device/xiaomi/fog
-rm -rf device/xiaomi/fog-kernel
+#rm -rf device/xiaomi/fog-kernel
 
-repo init -u https://github.com/VoltageOS/manifest.git --depth 1 -b 16.2 --git-lfs
-rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-VoltageOS .repo/local_manifests
+##repo init -u https://github.com/VoltageOS/manifest.git --depth 1 -b 16.2 --git-lfs
+##rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-VoltageOS .repo/local_manifests
 
 #echo "repo sync"
-/opt/crave/resync.sh
+##/opt/crave/resync.sh
 # signing key
-curDir=`pwd`
-cd vendor/voltage-priv/keys
-./keys.sh
-cd $curDir
+##curDir=`pwd`
+##cd vendor/voltage-priv/keys
+##./keys.sh
+##cd $curDir
 
 #rm -rf bionic && git clone  https://github.com/VoltageOS/bionic -b 16.2 bionic &&  cd bionic && git checkout 9bc94b544244ffab12aa05cd670a135ebdda45ab
 #cd $curDir
@@ -27,7 +27,7 @@ cd $curDir
 ##wget https://github.com/SourceLab081/uploadz/releases/download/v0.1.8/voltage.devices && mv voltage.devices vendor/voltage/
 #cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
 #cd kernel/xiaomi/fog &&	rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy_susfs && cd $curDir
-rm -rf kernel/xiaomi/fog && git clone  -b fog_new --depth 1 --recurse-submodules https://github.com/SourceLab081/greenforce kernel/xiaomi/fog
+##rm -rf kernel/xiaomi/fog && git clone  -b fog_new --depth 1 --recurse-submodules https://github.com/SourceLab081/greenforce kernel/xiaomi/fog
 
 #if [ ! -f script_sch2.sh ]; then
 #   wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/script_sch2.sh
