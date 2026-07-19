c5c61fd4ed1c7531592f27ae93b061b3d762ce50
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-07-19 07:06:16 +0700

    add 17-los

---
+++ 17-los.sh
@@ -0,0 +1,38 @@
+
+export TZ=Asia/Jakarta
+export BUILD_USERNAME=sourceslab062
+export BUILD_HOSTNAME=foss
+echo "start date = `date`"
+
+repo init --depth 1 -u https://github.com/Pixelify-AOSP/platform_manifest -b 17 --git-lfs
+rm -rf .repo/local_manifests && git clone  https://github.com/SourceLab081/local_manifests --depth 1 -b 17-los .repo/local_manifests
+echo "repo sync"
+/opt/crave/resync.sh
+#wget https://raw.githubusercontent.com/accupara/docker-images/master/aosp/common/resync.sh
+ #. resync.sh
+# signing key
+if [ ! -d vendor/extra ]; then
+   mkdir -p vendor/extra
+   cd vendor/extra && wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.8/sign.zip && unzip sign.zip && rm sign.zip
+   cd ../..
+fi
+
+
+# wget https://github.com/SourceLab081/uploadz/releases/download/v0.1.5/280dpi && mv 280dpi vendor/aicp/charger/images/ 
+
+curDir=`pwd`
+#cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
+#cd kernel/xiaomi/fog &&	rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy_susfs && cd $curDir
+rm -rf kernel/xiaomi/fog && git clone  -b fog_new --depth 1 --recurse-submodules https://github.com/SourceLab081/greenforce kernel/xiaomi/fog
+
+
+echo "envsetup.sh"
+. build/envsetup.sh
+#export ALLOW_MISSING_DEPENDENCIES=true 
+#export SELINUX_IGNORE_NEVERALLOWS=true
+#echo "breakfast/lunch"
+#breakfast fog eng
+#lunch aicp_fog-eng
+make installclean
+echo "Breakfast + Build the code"
+brunch fog user
