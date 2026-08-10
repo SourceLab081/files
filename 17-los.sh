020f478fc06693845769ae41b2f02ee9f1a06e42
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-08-10 10:31:54 +0700

    update

--- 17-los.sh
+++ 17-los.sh
@@ -4,10 +4,10 @@ export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 echo "start date = `date`"
 
-##repo init --depth 1 -u https://github.com/Pixelify-AOSP/platform_manifest -b 17 --git-lfs
-##rm -rf .repo/local_manifests && git clone  https://github.com/SourceLab081/local_manifests --depth 1 -b 17-los .repo/local_manifests
-##echo "repo sync"
-##/opt/crave/resync.sh
+repo init --depth 1 -u https://github.com/Pixelify-AOSP/platform_manifest -b 17 --git-lfs
+rm -rf .repo/local_manifests && git clone  https://github.com/SourceLab081/local_manifests --depth 1 -b 17-los .repo/local_manifests
+echo "repo sync"
+/opt/crave/resync.sh
 #wget https://raw.githubusercontent.com/accupara/docker-images/master/aosp/common/resync.sh
  #. resync.sh
 # signing key
@@ -23,9 +23,9 @@ fi
 curDir=`pwd`
 #cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
 #cd kernel/xiaomi/fog &&	rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy_susfs && cd $curDir
-##rm -rf kernel/xiaomi/fog && git clone  -b fog_new --depth 1 --recurse-submodules https://github.com/SourceLab081/greenforce kernel/xiaomi/fog
+rm -rf kernel/xiaomi/fog && git clone  -b fog_new --depth 1 --recurse-submodules https://github.com/SourceLab081/greenforce kernel/xiaomi/fog
 
-export JAVA_TOOL_OPTIONS="-Xmx2112m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -XX:+UseG1GC -Dfile.encoding=UTF-8"	
+#export JAVA_TOOL_OPTIONS="-Xmx2112m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -XX:+UseG1GC -Dfile.encoding=UTF-8"	
 #export JACK_SERVER_VM_ARGUMENTS="-Xmx4g"
 #export JAVA_TOOL_OPTIONS="-Xmx4g -XX:+UseG1GC -Dfile.encoding=UTF-8"
 #export ANDROID_RAM_INDEX=4096
@@ -42,16 +42,60 @@ export JAVA_TOOL_OPTIONS="-Xmx2112m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDump
 #echo 32G | sudo tee /sys/block/zram0/disksize
 #sudo mkswap /dev/zram0
 #sudo swapon /dev/zram0
-
+PACKAGE_NAME=Pixelify-AOSP
 echo "envsetup.sh"
 . build/envsetup.sh
 #export ALLOW_MISSING_DEPENDENCIES=true 
 #export SELINUX_IGNORE_NEVERALLOWS=true
 #echo "breakfast/lunch"
 #breakfast fog eng
-lunch fog-cp2a-user
+lunch fog-cp2a-user 
 #mka bacon
-make -j14 
+mka installclean
+
+#A17 soong oom killed workaround.
+#soong_build is incremental ie. it continues where it was before killed.
+#'m nothing' does all things needed before actual build begins.
+echo "Build $PACKAGE_NAME starting soong. "
+START_SECONDS=$(date +%s)
+(
+  sleep 3000; # 50 minutes 
+  #curl -s -X POST $TG_URL -d chat_id=$TG_CID -d text="crave.io build failed. soong timed out after limit. harakiri. `date`. JJ_SPEC:$JJ_SPEC" > /dev/null 2>&1 ;
+  #curl -s -d "crave.io build failed. soong timed out after limit. harakiri. `date`. JJ_SPEC:$JJ_SPEC" "ntfy.sh/$NTFYSUB" > /dev/null 2>&1 ;
+  echo "crave.io build failed. soong timed out after limit. harakiri. `date`"
+  #rm -rf /tmp/src/android/vendor/lineage-priv ;
+  kill -9 $$
+) &
+SLEEPID=$!
+export GOMEMLIMIT=52GiB GOGC=20 GODEBUG="gctrace=1" GOMAXPROCS=12
+for i in 1 2 3 4 5 6 7 8; do
+  NOW_SECONDS=$(date +%s)
+  USED_SECONDS=$((NOW_SECONDS - START_SECONDS))
+  USED_MINUTES=$((USED_SECONDS / 60))
+  echo "Build $PACKAGE_NAME trying soong. try $i. $USED_MINUTES minutes."
+  if [[ $USED_SECONDS -ge 2700 ]]; then # 45 minutes
+    echo "Build $PACKAGE_NAME failed. soong timed out. $i - 1 tries. $USED_MINUTES minutes."
+    kill -9 $SLEEPID
+    false # check_fail
+  fi    
+  if m nothing; then
+    NOW_SECONDS=$(date +%s)
+    USED_SECONDS=$((NOW_SECONDS - START_SECONDS))
+    USED_MINUTES=$((USED_SECONDS / 60))
+    echo "Build $PACKAGE_NAME soong success. $i tries. $USED_MINUTES minutes."
+    unset GOMEMLIMIT GOGC GODEBUG GOMAXPROCS
+    kill -9 $SLEEPID
+    break
+  fi
+  if [[ $i -eq 8 ]]; then
+    echo "Build $PACKAGE_NAME soong failed. $i tries. $USED_MINUTES minutes."
+    kill -9 $SLEEPID
+    false  #check_fail
+  fi 
+done
+unset GOMEMLIMIT GOGC GODEBUG GOMAXPROCS
+
+make -j$(nproc --all) 
 #make installclean
 #echo "Breakfast + Build the code"
 #brunch fog user
