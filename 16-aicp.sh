348336ee4ff0d968c811855f67b5c84bd17c880c
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-08-18 08:14:50 +0700

    Update 16-aicp.sh

--- 16-aicp.sh
+++ 16-aicp.sh
@@ -4,6 +4,8 @@ export BUILD_USERNAME=sourceslab062
 export BUILD_HOSTNAME=foss
 echo "start date = `date`"
 
+free -h
+nproc --all
 repo init --depth 1 -u https://github.com/AICP/platform_manifest.git -b w16.2 --git-lfs
 rm -rf .repo/local_manifests && git clone  https://github.com/SourceLab081/local_manifests --depth 1 -b 16-aicp .repo/local_manifests
 echo "repo sync"
