92f3e6c2efd7bf71ef9503ccc578f6728e1deac8
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-09-04 16:31:45 +0700

    Update 17-vos.sh

--- 17-vos.sh
+++ 17-vos.sh
@@ -154,7 +154,7 @@ if [[ "${BUILD_SUCCESS}" == "1" ]]; then
     if [ ! -f github-release ]; then
        wget https://github.com/github-release/github-release/releases/download/v0.10.0/linux-amd64-github-release.bz2&&bunzip2 linux-amd64-github-release.bz2&&mv linux-amd64-github-release github-release && chmod +x github-release
     fi
-    if [ ! -f send_file ]; then
+    if [ ! -f send_file.sh ]; then
        echo './github-release upload \
        -s $secret_num \
        -u SourceLab081 \
@@ -167,7 +167,7 @@ if [[ "${BUILD_SUCCESS}" == "1" ]]; then
        ZIP_FILE=$(find . -maxdepth 1 -type f -name "voltage-6*.zip" \
        -printf '%T@ %p\n' | sort -nr | head -n 1 | cut -d' ' -f2-)
        info "Send $ZIP_FILE to github"
-       . send_file $ZIP_FILE
+       . send_file.sh $ZIP_FILE
     fi
 else
     fail "VoltageOS build failed"
