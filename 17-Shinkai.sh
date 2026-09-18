045be3938dd14931e78d8db8fed2770e30b45528
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-09-18 08:12:02 +0700

    Update 17-Shinkai.sh

--- 17-Shinkai.sh
+++ 17-Shinkai.sh
@@ -75,7 +75,7 @@ if [[ "$first" = "yes" || "$update" = "yes" ]]; then
    source_repo
    
    repo_sync_crave 
-   
+   repo sync -c -j$(nproc --all) --force-sync --force-remove-dirty --no-clone-bundle --no-tags
    repo_sync_crave 
    # Fix for the “memory stall” error when building Soong
    wget https://github.com/yaap-17-stone/build_soong/raw/f9c27b0b9298f6eeee9a850346e0a646c3eaeb87/cmd/soong_build/main.go && mv main.go build/soong/cmd/soong_build/
